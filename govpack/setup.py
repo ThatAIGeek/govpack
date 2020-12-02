085be309d495fddee2c7e427b5c571ee09160905
Yaroslav Khoruzhenko <yarusx@gmail.com>
2020-12-02 20:13:50 +0200

    Just finished with a test pkg that consists of 3 zip files with medical financial datasets. I was following this guide: https://packaging.python.org/tutorials/packaging-projects/
    Uploaded it into a test server. You could check here: 
    https://test.pypi.org/project/govpack-yarusx/0.0.1/
    
    By this time I guess this pack is useless or idk how to operate zip 
    files inside.

---
+++ govpack/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="govpack-yarusx", # Replace with your own username
+    version="0.0.1",
+    author="Yaroslav Khoruzhenko",
+    author_email="yarusx@gmail.com",
+    description="A package with a useful data from https://data.gov.ua/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ThatAIGeek/govpack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
