52a2d2af4d7e066dc1dfae7605a96cf71ebd75b6
Yaroslav Khoruzhenko <yarusx@gmail.com>
2020-12-17 18:51:38 +0200

    Redundant initialization of med_api has been simplified

--- govpack/__init__.py
+++ govpack/__init__.py
@@ -1,7 +1,6 @@
 from .apis import *
 from .create_var import *
 
-# med_data = get_apis.create_apis(med_apis.apis_list)
-med_api0 = Med_apis.med_api0.value
-med_api1 = Med_apis.med_api1.value
-med_api2 = Med_apis.med_api2.value
+med_api0 = api+list(med_dict.values())[0]
+med_api1 = api+list(med_dict.values())[1]
+med_api2 = api+list(med_dict.values())[2]
