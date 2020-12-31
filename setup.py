425b727c923107a94835a5b7d0950b2afc651247
Yaroslav Khoruzhenko <yarusx@gmail.com>
2020-12-31 18:21:29 +0200

    'install_requires' added to step.py. tested - works

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="govpack-yarusx", # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="Yaroslav Khoruzhenko",
     author_email="yarusx@gmail.com",
     description="A package that helps to create pandas variables from a public datasets: https://data.gov.ua/ ",
@@ -19,4 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=['pandas', 'xlrd==1.2.0'],
 )
